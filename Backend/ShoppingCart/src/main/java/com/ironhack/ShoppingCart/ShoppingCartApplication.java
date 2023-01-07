3e316c79a2caac9a29f4609cca89e1c6125ffca3
Ariana7 <angela07rosales@gmail.com>
2023-01-07 14:05:25 +0100

    backend

---
+++ Backend/ShoppingCart/src/main/java/com/ironhack/ShoppingCart/ShoppingCartApplication.java
@@ -0,0 +1,12 @@
+package com.ironhack.ShoppingCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class ShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartApplication.class, args);
+	}
+
+}
